9f9e7dbe19c66a56f1a092b92eaafc9e13422a58
rezaarezvan <rezvanr2378@gmail.com>
2023-06-29 21:21:42 +0200

    Update: CNN; Using JAX;

---
+++ CNN/main.py
@@ -0,0 +1,199 @@
+import jax
+import jax.numpy as jnp
+import optax  # https://github.com/deepmind/optax
+import torch  # https://pytorch.org
+import torchvision  # https://pytorch.org
+# https://github.com/google/jaxtyping
+from jaxtyping import Array, Float, Int, PyTree
+import equinox as eqx
+
+
+# Hyperparameters
+BATCH_SIZE = 64
+LEARNING_RATE = 1e-4
+STEPS = 300
+PRINT_EVERY = 30
+SEED = 69420
+
+key = jax.random.PRNGKey(SEED)
+
+normalise_data = torchvision.transforms.Compose(
+    [
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize((0.5,), (0.5,)),
+    ]
+)
+train_dataset = torchvision.datasets.MNIST(
+    "MNIST",
+    train=True,
+    download=True,
+    transform=normalise_data,
+)
+test_dataset = torchvision.datasets.MNIST(
+    "MNIST",
+    train=False,
+    download=True,
+    transform=normalise_data,
+)
+trainloader = torch.utils.data.DataLoader(
+    train_dataset, batch_size=BATCH_SIZE, shuffle=True
+)
+testloader = torch.utils.data.DataLoader(
+    test_dataset, batch_size=BATCH_SIZE, shuffle=True
+)
+
+dummy_x, dummy_y = next(iter(trainloader))
+dummy_x = dummy_x.numpy()
+dummy_y = dummy_y.numpy()
+print(dummy_x.shape)  # 64x1x28x28
+print(dummy_y.shape)  # 64
+print(dummy_y)
+
+
+class CNN(eqx.Module):
+    layers: list
+
+    def __init__(self, key):
+        key1, key2, key3, key4 = jax.random.split(key, 4)
+        # Standard CNN setup: convolutional layer, followed by flattening,
+        # with a small MLP on top.
+        self.layers = [
+            eqx.nn.Conv2d(1, 3, kernel_size=4, key=key1),
+            eqx.nn.MaxPool2d(kernel_size=2),
+            jax.nn.relu,
+            jnp.ravel,
+            eqx.nn.Linear(1728, 512, key=key2),
+            jax.nn.sigmoid,
+            eqx.nn.Linear(512, 64, key=key3),
+            jax.nn.relu,
+            eqx.nn.Linear(64, 10, key=key4),
+            jax.nn.log_softmax,
+        ]
+
+    def __call__(self, x: Float[Array, "1 28 28"]) -> Float[Array, "10"]:
+        for layer in self.layers:
+            x = layer(x)
+        return x
+
+
+key, subkey = jax.random.split(key, 2)
+model = CNN(subkey)
+
+
+# print(model)
+
+def loss(
+    model: CNN, x: Float[Array, "batch 1 28 28"], y: Int[Array, " batch"]
+) -> Float[Array, ""]:
+    # Our input has the shape (BATCH_SIZE, 1, 28, 28), but our model operations on
+    # a single input input image of shape (1, 28, 28).
+    #
+    # Therefore, we have to use jax.vmap, which in this case maps our model over the
+    # leading (batch) axis.
+    pred_y = jax.vmap(model)(x)
+    return cross_entropy(y, pred_y)
+
+
+def cross_entropy(
+    y: Int[Array, " batch"], pred_y: Float[Array, "batch 10"]
+) -> Float[Array, ""]:
+    # y are the true targets, and should be integers 0-9.
+    # pred_y are the log-softmax'd predictions.
+    pred_y = jnp.take_along_axis(pred_y, jnp.expand_dims(y, 1), axis=1)
+    return -jnp.mean(pred_y)
+
+
+# Example loss
+loss_value = loss(model, dummy_x, dummy_y)
+print(loss_value.shape)  # scalar loss
+# Example inference
+output = jax.vmap(model)(dummy_x)
+print(output.shape)  # batch of predictions
+
+value, grads = eqx.filter_value_and_grad(loss)(model, dummy_x, dummy_y)
+print(value)
+
+loss = eqx.filter_jit(loss)  # JIT our loss function from earlier!
+
+
+@eqx.filter_jit
+def compute_accuracy(
+    model: CNN, x: Float[Array, "batch 1 28 28"], y: Int[Array, " batch"]
+) -> Float[Array, ""]:
+    """This function takes as input the current model
+    and computes the average accuracy on a batch.
+    """
+    pred_y = jax.vmap(model)(x)
+    pred_y = jnp.argmax(pred_y, axis=1)
+    return jnp.mean(y == pred_y)
+
+
+def evaluate(model: CNN, testloader: torch.utils.data.DataLoader):
+    """This function evaluates the model on the test dataset,
+    computing both the average loss and the average accuracy.
+    """
+    avg_loss = 0
+    avg_acc = 0
+    for x, y in testloader:
+        x = x.numpy()
+        y = y.numpy()
+        # Note that all the JAX operations happen inside `loss` and `compute_accuracy`,
+        # and both have JIT wrappers, so this is fast.
+        avg_loss += loss(model, x, y)
+        avg_acc += compute_accuracy(model, x, y)
+    return avg_loss / len(testloader), avg_acc / len(testloader)
+
+
+print(evaluate(model, testloader))
+
+optim = optax.adamw(LEARNING_RATE)
+
+
+def train(
+    model: CNN,
+    trainloader: torch.utils.data.DataLoader,
+    testloader: torch.utils.data.DataLoader,
+    optim: optax.GradientTransformation,
+    steps: int,
+    print_every: int,
+) -> CNN:
+    # Just like earlier: It only makes sense to train the arrays in our model,
+    # so filter out everything else.
+    opt_state = optim.init(eqx.filter(model, eqx.is_array))
+
+    # Always wrap everything -- computing gradients, running the optimiser, updating
+    # the model -- into a single JIT region. This ensures things run as fast as
+    # possible.
+    @eqx.filter_jit
+    def make_step(
+        model: CNN,
+        opt_state: PyTree,
+        x: Float[Array, "batch 1 28 28"],
+        y: Int[Array, " batch"],
+    ):
+        loss_value, grads = eqx.filter_value_and_grad(loss)(model, x, y)
+        updates, opt_state = optim.update(grads, opt_state, model)
+        model = eqx.apply_updates(model, updates)
+        return model, opt_state, loss_value
+
+    # Loop over our training dataset as many times as we need.
+    def infinite_trainloader():
+        while True:
+            yield from trainloader
+
+    for step, (x, y) in zip(range(steps), infinite_trainloader()):
+        # PyTorch dataloaders give PyTorch tensors by default,
+        # so convert them to NumPy arrays.
+        x = x.numpy()
+        y = y.numpy()
+        model, opt_state, train_loss = make_step(model, opt_state, x, y)
+        if (step % print_every) == 0 or (step == steps - 1):
+            test_loss, test_accuracy = evaluate(model, testloader)
+            print(
+                f"{step=}, train_loss={train_loss.item()}, "
+                f"test_loss={test_loss.item()}, test_accuracy={test_accuracy.item()}"
+            )
+    return model
+
+
+model = train(model, trainloader, testloader, optim, STEPS, PRINT_EVERY)
